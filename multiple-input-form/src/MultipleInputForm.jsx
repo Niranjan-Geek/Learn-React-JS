41467c63b11d86851368d971393a41d3ae8fcd0e
Niranjan-Geek <niranjangeek435@gmail.com>
2023-04-11 12:47:23 +0530

    Update 11-04-2023

--- multiple-input-form/src/MultipleInputForm.jsx
+++ multiple-input-form/src/MultipleInputForm.jsx
@@ -8,31 +8,38 @@ const MultipleInputForm = () => {
     });
 
     const inputEvent = (event) => {
-        const value = event.target.value;
-        const inputName = event.target.name;
+        // const value = event.target.value;
+        // const inputName = event.target.name;
+
+        const {name, value} = event.target; //Distructuring
 
         setFullName((preValue) => {
-            if (inputName === "fname"){
-                return{
-                    fname: value,
-                    lname: preValue.lname,
-                    email: preValue.email,
-                }
+            return{
+                ...preValue,
+                [name]: value,
             }
-            else if (inputName === "lname"){
-                return{
-                    fname: preValue.fname,
-                    lname: value,
-                    email: preValue.email,
-                }
-            }
-            else if (inputName === "email"){
-                return{
-                    fname: preValue.fname,
-                    lname: preValue.lname,
-                    email: value,
-                }
-            }
+
+            // if (inputName === "fname"){
+            //     return{
+            //         fname: value,
+            //         lname: preValue.lname,
+            //         email: preValue.email,
+            //     }
+            // }
+            // else if (inputName === "lname"){
+            //     return{
+            //         fname: preValue.fname,
+            //         lname: value,
+            //         email: preValue.email,
+            //     }
+            // }
+            // else if (inputName === "email"){
+            //     return{
+            //         fname: preValue.fname,
+            //         lname: preValue.lname,
+            //         email: value,
+            //     }
+            // }
         })
     }
 
@@ -50,11 +57,13 @@ const MultipleInputForm = () => {
                     <input type="text" onChange={inputEvent} name="fname" value={fullName.fname} placeholder="Enter First Name" />
                     <input type="text" onChange={inputEvent} name="lname" value={fullName.lname} placeholder="Enter Last Name" />
                     <input type="email" onChange={inputEvent} name="email" value={fullName.email} placeholder="Enter Email Address" />
+                    <input type="text" onChange={inputEvent} name="phone" value={fullName.phone} placeholder="Enter Phone Number" />
                     <button type="submit">Login</button>
                 </form>
                 <p>First Name is: <span>{fullName.fname}</span></p>
                 <p>Last Name is: <span>{fullName.lname}</span></p>
                 <p>Email Address is: <span>{fullName.email}</span></p>
+                <p>Phone Number is: <span>{fullName.phone}</span></p>
             </div>
         </>
     )
