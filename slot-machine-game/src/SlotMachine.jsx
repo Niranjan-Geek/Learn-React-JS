e72e1f80a1697de1cc621afe231c2aa84db7faf2
Niranjan-Geek <niranjangeek435@gmail.com>
2023-04-12 16:38:08 +0530

    Update 12-04-2023

--- slot-machine-game/src/SlotMachine.jsx
+++ slot-machine-game/src/SlotMachine.jsx
@@ -3,11 +3,12 @@ import SlotM from "./SlotM";
 import img1 from "./images/diamond.png";
 import img2 from "./images/red-diamond.png";
 import img3 from "./images/diamond.png";
+import CasinoIcon from '@mui/icons-material/Casino';
 
 const SlotMachine = () => {
     return(
         <>
-            <h1 className="title">Slot Machine Game</h1>
+            <h1 className="title">Slot Machine Game <CasinoIcon fontSize="large" /></h1>
             <div className="slot-machine">
                 <SlotM srcA={img1} srcB={img2} srcC={img3}/>
                 <SlotM srcA={img1} srcB={img3} srcC={img1}/>
