d4cc9a5eb593b0dc6470011e0d0245290e98ff0e
Niranjan-Geek <niranjangeek435@gmail.com>
2023-04-25 14:51:33 +0530

    update 25-04-2023

--- router-dom/src/Menu.jsx
+++ router-dom/src/Menu.jsx
@@ -6,10 +6,10 @@ const Menu = () => {
     <>
       <nav className="navigation">
         <ul>
-            <li><NavLink exact activeClassName="active" to="/">Home</NavLink></li>
-            <li><NavLink exact activeClassName="active" to="/about">About</NavLink></li>
-            <li><NavLink exact activeClassName="active" to="/contact">Contact</NavLink></li>
-            <li><NavLink exact activeClassName="active" to="/use-params">Use Params</NavLink></li>
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink exact to="/about">About</NavLink></li>
+            <li><NavLink exact to="/contact">Contact</NavLink></li>
+            <li><NavLink exact to="/use-params">Use Params</NavLink></li>
         </ul>
       </nav>
     </>
